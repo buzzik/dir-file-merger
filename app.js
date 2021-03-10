04a61b38931cb50a440556bef702c8f214569c36
mr4k <buzzik13@gmail.com>
2021-03-10 09:46:38 +0200

    end-line string parameter added

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const fs = require('fs');
 const { promisify } = require('util');
 const readdir = promisify(fs.readdir);
 const { writeFile } = require('fs').promises;
-async function mergeFiles(dir, output) {
+async function mergeFiles(dir, output, endline) {
+  endline = endline || '';
   let result = '';
   const names = await readdir(dir);
   names.forEach((file) => {
     const filePath = `${dir}/${file}`;
     const data = fs.readFileSync(filePath, 'utf8');
-    result += `${data}`;
+    result += `${data}${endline}`;
   });
   await writeFile(output, result);
   return 'file saved';
